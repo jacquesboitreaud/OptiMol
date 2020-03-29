88e25be8e5700827025b32988d73ef01d1ce2cf5
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-03-29 18:46:07 +0200

    added scaffolds parsing

--- data_processing/chem_props.py
+++ data_processing/chem_props.py
@@ -7,35 +7,49 @@ Created on Thu Oct 24 15:26:06 2019
 Compute chemical properties for a dataframe
 """
 import numpy as np
+import argparse
 from rdkit import Chem
 from rdkit.Chem import Draw, QED, Crippen, Descriptors, rdMolDescriptors, GraphDescriptors
 import pandas as pd
 
-path_to_file = 'C:/Users/jacqu/Documents/GitHub/vina_docking/data/scored_exp/drd3_dude_scored_64_1mode.csv'
+if __name__=='__main__':
 
-df=pd.read_csv(path_to_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-csv', '--csv', help="path to molecules dataset. Column with smiles labeled 'can'", type=str, default='../data/moses_train.csv')
+    parser.add_argument('-n', '--cutoff', help="cutoff N molecules. -1 for all in csv", type=int, default=10)
     
-smiles = list(df['can'])
-d={}
-prop_names=['QED','logP','molWt']
-for name in prop_names : 
-    d[f'{name}']=[]
+    # =======
 
-
-for i,s in enumerate(smiles) : 
-    if(i%1000==0):
-        print(i)
-    m=Chem.MolFromSmiles(s)
-    d['QED'].append(QED.default(m))
-    d['logP'].append(Crippen.MolLogP(m))
-    d['molWt'].append(Descriptors.MolWt(m))
+    args=parser.parse_args()
     
-
-for k in d.keys():
-    df[k]=pd.Series(d[k], index = df.index)
+    if(args.cutoff>0):
+        data = pd.read_csv(args.csv, nrows = args.cutoff)
+    else:
+        data = pd.read_csv(args.csv)
     
-# Drop lines with Nan properties
-df = df.dropna(axis=0)
-
-df.to_csv(path_to_file)
+    smiles = list(data['can'])
+    d={}
+    prop_names=['QED','logP','molWt']
+    for name in prop_names : 
+        d[f'{name}']=[]
+    
+    print(f'>>> computing {prop_names} for {len(smiles)} molecules')
+    for i,s in enumerate(smiles) : 
+        if(i%1000==0 and i >0):
+            print(i)
+        m=Chem.MolFromSmiles(s)
+        d['QED'].append(QED.default(m))
+        d['logP'].append(Crippen.MolLogP(m))
+        d['molWt'].append(Descriptors.MolWt(m))
+        
+    
+    for k in d.keys():
+        data[k]=pd.Series(d[k], index = data.index)
+        
+    # Drop lines with Nan properties
+    data = data.dropna(axis=0, subset = prop_names)
+    
+    savename = args.csv[:-4]+'_props.csv'
+    print(f'>>> saving dataframe to {savename}')
+    data.to_csv(savename)
     
--- data_processing/scaffolds.py
+++ data_processing/scaffolds.py
@@ -4,38 +4,67 @@ Created on Thu Oct 24 15:26:06 2019
 
 @author: jacqu
 
-Compute chemical properties for a dataframe
+Finds and stores unique scaffolds in dataset. 
+Scaffolds are Murcko scaffolds, computed using RDKIT module MurckoScaffold.MurckoScaffoldSmiles. 
+
 """
 import numpy as np
 from rdkit import Chem
-from rdkit.Chem import Draw, QED, Crippen, Descriptors, rdMolDescriptors, GraphDescriptors
+from rdkit.Chem.Scaffolds import MurckoScaffold
 import pandas as pd
+import argparse
 
-path_to_file = 'C:/Users/jacqu/Documents/GitHub/vina_docking/data/scored_exp/drd3_dude_scored_64_1mode.csv'
+if __name__=='__main__':
 
-df=pd.read_csv(path_to_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-csv', '--csv', help="path to molecules dataset. Column with smiles labeled 'can'", type=str, default='../data/moses_train.csv')
+    parser.add_argument('-n', '--cutoff', help="cutoff N molecules. -1 for all in csv", type=int, default=10)
+    # =======
+
+    args=parser.parse_args()
     
-smiles = list(df['can'])
-d={}
-prop_names=['QED','logP','molWt']
-for name in prop_names : 
-    d[f'{name}']=[]
+    if(args.cutoff>0):
+        data = pd.read_csv(args.csv, nrows = args.cutoff)
+    else:
+        data = pd.read_csv(args.csv)
+        
+    try:
+        smiles = list(data['can'])
+    except:
+        print('No column with label "can" found in csv file. Assuming canonical smiles are in column 0.')
+        smiles = list(data.iloc[0])
 
+    print('>>> Parsing ', len(smiles), ' molecules')
 
-for i,s in enumerate(smiles) : 
-    if(i%1000==0):
-        print(i)
-    m=Chem.MolFromSmiles(s)
-    d['QED'].append(QED.default(m))
-    d['logP'].append(Crippen.MolLogP(m))
-    d['molWt'].append(Descriptors.MolWt(m))
     
 
-for k in d.keys():
-    df[k]=pd.Series(d[k], index = df.index)
+    scaffolds_dict = {}
+    scaffolds = []
     
-# Drop lines with Nan properties
-df = df.dropna(axis=0)
-
-df.to_csv(path_to_file)
+    
+    for i,s in enumerate(smiles) : 
+        if (i%1000==0 and i>0):
+            print(i)
+        sc = MurckoScaffold.MurckoScaffoldSmiles(s)
+        scaffolds.append(sc)
+        if(sc not in scaffolds_dict.keys()):
+            scaffolds_dict[sc]=1
+        else:
+            scaffolds_dict[sc]+=1
+    
+    scaf, counts = [k for k,v in scaffolds_dict.items()], [v for k,v in scaffolds_dict.items()]
+    print(f'Found {len(scaf)} unique scaffolds in dataset')
+    output_d = {'smiles':scaf, 'counts':counts}
+        
+    # Scaffolds and counts 
+    u = pd.DataFrame.from_dict(output_d)
+    print(f'>>> Saving scaffolds and number of occurences to ~/unique_scaffolds.csv')
+    u.to_csv('unique_scaffolds.csv')
+        
+    # For each molecule, add its scaffold as a df column
+    data['scaffold']= pd.Series(scaffolds, index = data.index)
+    
+    savename = args.csv[:-4]+'_scaf.csv'
+    print(f'>>> saving dataframe to {savename}')
+    data.to_csv(savename)
     
