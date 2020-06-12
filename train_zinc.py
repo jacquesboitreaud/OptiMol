06a358b9c2e9dbcb46e61769582f4794bc2d4a2b
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-06-12 19:14:50 +0200

    big model zinc

--- train_zinc.py
+++ train_zinc.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
 from utils import ModelDumper, disable_rdkit_logging, setup, log_reconstruction
 from dgl_utils import send_graph_to_device
-from model import Model
+from model_zinc import Model
 from loss_func import VAELoss, weightedPropsLoss, affsRegLoss, affsClassifLoss
 from dataloaders.molDataset import molDataset, Loader
 
@@ -65,7 +65,8 @@ if __name__ == "__main__":
 
     # No need to change under this
 
-    parser.add_argument('--latent_size', type=int, default=56)  # size of latent code
+    parser.add_argument('--latent_size', type=int, default=128)  # size of latent code
+    
     parser.add_argument('--n_gcn_layers', type=int, default=3)  # number of gcn encoder layers (3 or 4?)
     parser.add_argument('--lr', type=float, default=1e-3)  # Initial learning rate
     parser.add_argument('--clip_norm', type=float, default=50.0)  # Gradient clipping max norm
@@ -94,8 +95,6 @@ if __name__ == "__main__":
 
     # Multitask :
     parser.add_argument('--no_props', action='store_false')  # No multitask props
-    parser.add_argument('--no_aff', action='store_false')  # No multitask aff
-    parser.add_argument('--bin_affs', action='store_true')  # Binned discretized affs or true values
     
     parser.add_argument('--gpu_id', type=int, default=0)  # run model on cuda:{id} if multiple gpus on server
 
@@ -106,26 +105,20 @@ if __name__ == "__main__":
     logdir, modeldir = setup(args.name, permissive=True)
     dumper = ModelDumper(dumping_path=os.path.join(modeldir, 'params.json'), argparse=args)
 
-    use_props, use_affs = True, True
+    use_props, use_affs = True, False
     if args.no_props:
         use_props = False
-    if args.no_aff:
-        use_affs = False
+    
     # Multitasking : properties and affinities should be in input dataset
     if use_props:
         properties = ['QED', 'logP', 'molWt']
     else:
         properties = []
     props_weights = [1e3, 1e2, 1]
-    if use_affs:
-        targets = ['drd3']
-    else:
-        targets = []
-    a_weight = 1e2  # Weight for affinity regression loss
 
-    if args.bin_affs:
-        targets = [t + '_binned' for t in targets]  # use binned scores columns
-        classes_weights = torch.tensor([0., 1., 1.])  # class weights
+
+    targets = []
+    
 
     writer = SummaryWriter(logdir)
     disable_rdkit_logging()  # function from utils to disable rdkit logs
@@ -148,13 +141,14 @@ if __name__ == "__main__":
     params = {'features_dim': loaders.dataset.emb_size,  # node embedding dimension
               'num_rels': loaders.num_edge_types,
               'gcn_layers': args.n_gcn_layers,
-              'gcn_hdim': 32,
+              'gcn_hdim': 64,
+              'gru_hdim':512,
+              'gru_dropout':0.2,
               'l_size': args.latent_size,
               'voc_size': loaders.dataset.n_chars,
               'max_len': loaders.dataset.max_len,
               'N_properties': len(properties),
               'N_targets': len(targets),
-              'binned_scores': args.bin_affs,
               'device': device,
               'index_to_char': loaders.dataset.index_to_char,
               'props': properties,
@@ -207,35 +201,23 @@ if __name__ == "__main__":
             graph = send_graph_to_device(graph, device)
             if use_props:
                 p_target = p_target.to(device).view(-1, len(properties))
-            if use_affs:
-                a_target = a_target.to(device)
 
             # Forward passs
-            mu, logv, _, out_smi, out_p, out_a = model(graph, smiles, tf=tf_proba)
+            mu, logv, _, out_smi, out_p = model(graph, smiles, tf=tf_proba)
 
             # Compute loss terms : change according to multitask setting
             rec, kl = VAELoss(out_smi, smiles, mu, logv)
 
-            if not use_affs and not use_props:  # VAE only
-                pmse, amse = torch.tensor(0), torch.tensor(0)
-            elif use_props and not use_affs:
+            if not use_props:  # VAE only
+                pmse = torch.tensor(0)
+            elif use_props :
                 pmse = weightedPropsLoss(p_target, out_p, props_weights)
-                amse = torch.tensor(0)
-            elif use_affs:
-                if args.bin_affs:
-                    amse = affsClassifLoss(a_target, out_a, classes_weights)
-                else:
-                    amse = affsRegLoss(a_target, out_a, a_weight)
-                if use_props:
-                    pmse = weightedPropsLoss(p_target, out_p, props_weights)
-                else:
-                    pmse = torch.tensor(0)
 
             # COMPOSE TOTAL LOSS TO BACKWARD
             if total_steps < args.warmup:  # Only reconstruction (warmup)
                 t_loss = rec
             else:
-                t_loss = rec + beta * kl + pmse + amse
+                t_loss = rec + beta * kl + pmse 
 
             optimizer.zero_grad()
             t_loss.backward()
@@ -258,15 +240,14 @@ if __name__ == "__main__":
             if total_steps % args.print_iter == 0:
                 now = time()
                 print(
-                    f'Opt step {total_steps}, rec: {rec.item():.2f}, kl: {beta * kl.item():.2f}, props mse: {pmse.item():.2f}, aff mse: {amse.item():.2f}')
+                    f'Opt step {total_steps}, rec: {rec.item():.2f}, kl: {beta * kl.item():.2f}, props mse: {pmse.item():.2f}')
                 print('Time elapsed : ', now-start)
                 
                 writer.add_scalar('BatchRec/train', rec.item(), total_steps)
                 writer.add_scalar('BatchKL/train', kl.item(), total_steps)
                 if use_props:
                     writer.add_scalar('BatchPropMse/train', pmse.item(), total_steps)
-                if use_affs:
-                    writer.add_scalar('BatchAffMse/train', amse.item(), total_steps)
+
 
             if args.print_smiles_iter > 0 and total_steps % args.print_smiles_iter == 0:
                 _, out_chars = torch.max(out_smi.detach(), dim=1)
@@ -280,7 +261,7 @@ if __name__ == "__main__":
                 quality = 100. * torch.mean(differences)
                 quality = quality.detach().cpu()
                 writer.add_scalar('quality/train', quality.item(), total_steps)
-                print('fraction of correct characters at reconstruction : ', quality.item())
+                print('fraction of correct characters at reconstruction // train : ', quality.item())
 
             if total_steps % args.save_iter == 0:
                 model.cpu()
@@ -291,7 +272,6 @@ if __name__ == "__main__":
             epoch_train_rec += rec.item()
             epoch_train_kl += kl.item()
             epoch_train_pmse += pmse.item()
-            epoch_train_amse += amse.item()
 
         # Validation pass
         model.eval()
@@ -304,33 +284,21 @@ if __name__ == "__main__":
 
                 if use_props:
                     p_target = p_target.to(device).view(-1, len(properties))
-                if use_affs:
-                    a_target = a_target.to(device)
 
-                mu, logv, z, out_smi, out_p, out_a = model(graph, smiles, tf=tf_proba)
+                mu, logv, z, out_smi, out_p= model(graph, smiles, tf=tf_proba)
 
                 # Compute loss : change according to multitask
 
                 rec, kl = VAELoss(out_smi, smiles, mu, logv)
-                if not use_affs and not use_props:  # VAE only
-                    pmse, amse = torch.tensor(0), torch.tensor(0)
-                elif use_props and not use_affs:
+                if not use_props:  # VAE only
+                    pmse = torch.tensor(0)
+                elif use_props :
                     pmse = weightedPropsLoss(p_target, out_p, props_weights)
-                    amse = torch.tensor(0)
-                elif use_affs:
-                    if args.bin_affs:
-                        amse = affsClassifLoss(a_target, out_a, classes_weights)
-                    else:
-                        amse = affsRegLoss(a_target, out_a, a_weight)
-                    if use_props:
-                        pmse = weightedPropsLoss(p_target, out_p, props_weights)
-                    else:
-                        pmse = torch.tensor(0)
+
 
                 val_rec += rec.item()
                 val_kl += kl.item()
                 val_pmse += pmse.item()
-                val_amse += amse.item()
 
                 # Correctly reconstructed characters in first validation batch
                 if batch_idx == 0:
@@ -340,22 +308,19 @@ if __name__ == "__main__":
                     quality = 100. * torch.mean(differences)
                     quality = quality.detach().cpu()
                     writer.add_scalar('quality/valid', quality.item(), epoch)
-                    print('fraction of correct characters in first valid batch : ', quality.item())
+                    print('fraction of correct characters at reconstruction // validation : ', quality.item())
 
             # total Epoch losses
-            val_rec, val_kl, val_pmse, t_amse = val_rec / len(test_loader), \
+            val_rec, val_kl, val_pmse = val_rec / len(test_loader), \
                                                 val_kl / len(test_loader), \
-                                                val_pmse / len(test_loader), \
-                                                val_amse / len(test_loader)
+                                                val_pmse / len(test_loader)
 
-            epoch_train_rec, epoch__train_kl, epoch_train_pmse, epoch_amse = epoch_train_rec / len(train_loader), \
+            epoch_train_rec, epoch__train_kl, epoch_train_pmse = epoch_train_rec / len(train_loader), \
                                                                              epoch_train_kl / len(train_loader), \
-                                                                             epoch_train_pmse / len(train_loader), \
-                                                                             epoch_train_amse / len(train_loader)
+                                                                             epoch_train_pmse / len(train_loader)
 
-        print(f'[Chunk {epoch}/], batch valid. loss: rec: {val_rec:.2f}, '
-              f'kl: {beta * kl.item():.2f}, props mse: {val_pmse:.2f},'
-              f' aff mse: {val_amse:.2f}')
+        print(f'[Chunk {epoch}], batch valid. loss: rec: {val_rec:.2f}, '
+              f'kl: {beta * kl.item():.2f}, props mse: {val_pmse:.2f}')
 
         # Tensorboard logging
         writer.add_scalar('EpochRec/valid', val_rec, epoch)
@@ -366,7 +331,3 @@ if __name__ == "__main__":
         if use_props:
             writer.add_scalar('EpochPropLoss/valid', val_pmse, epoch)
             writer.add_scalar('EpochPropLoss/train', epoch_train_pmse, epoch)
-
-        if use_affs:
-            writer.add_scalar('EpochAffLoss/valid', val_amse, epoch)
-            writer.add_scalar('EpochAffLoss/train', epoch_train_amse, epoch)
