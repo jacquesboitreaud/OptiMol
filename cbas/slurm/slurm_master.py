3da601b704f8f903c0235303b54a67af51a655b0
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-07-06 12:21:31 +0200

    debug

--- cbas/slurm/slurm_master.py
+++ cbas/slurm/slurm_master.py
@@ -72,36 +72,36 @@ if __name__ == '__main__':
         """ Writes slurm sh scripts for CEDAR that will be called to launch slurm processes """
         # Sampler 
         with open(os.path.join(script_dir, 'slurm_sampler.sh'), 'w') as f :
-            f.write('#!/bin/sh')
-            f.write('#SBATCH --account=def-jeromew')
-            f.write('#SBATCH --time=00:05:00')
-            f.write('#SBATCH --job-name=sampler') 
-            f.write('#SBATCH --output=out_slurm/sampler_%A.out')
-            f.write('#SBATCH --error=out_slurm/sampler_%A.err')
-            f.write('#SBATCH --gres=gpu:1') # gpu request   
-            f.write('python sampler.py --prior_name $1 --name $2 --max_samples $3 --oracle $4 --cap_weights $5')
+            f.write('#!/bin/sh\n')
+            f.write('#SBATCH --account=def-jeromew\n')
+            f.write('#SBATCH --time=00:05:00\n')
+            f.write('#SBATCH --job-name=sampler\n') 
+            f.write('#SBATCH --output=out_slurm/sampler_%A.out\n')
+            f.write('#SBATCH --error=out_slurm/sampler_%A.err\n')
+            f.write('#SBATCH --gres=gpu:1\n') # gpu request   
+            f.write('python sampler.py --prior_name $1 --name $2 --max_samples $3 --oracle $4 --cap_weights $5\n')
             
         # Docker 
         with open(os.path.join(script_dir, 'slurm_docker.sh'), 'w') as f :
-            f.write('#!/bin/sh')
-            f.write('#SBATCH --account=def-jeromew')
-            f.write('#SBATCH --time=20:00:00')
-            f.write('#SBATCH --job-name=docker') 
-            f.write('#SBATCH --output=out_slurm/docker_%A.out')
-            f.write('#SBATCH --error=out_slurm/docker_%A.err')
-            f.write('#SBATCH --cpus-per-task=1')
-            f.write('#SBATCH --array=0-199')
+            f.write('#!/bin/sh\n')
+            f.write('#SBATCH --account=def-jeromew\n')
+            f.write('#SBATCH --time=20:00:00\n')
+            f.write('#SBATCH --job-name=docker\n') 
+            f.write('#SBATCH --output=out_slurm/docker_%A.out\n')
+            f.write('#SBATCH --error=out_slurm/docker_%A.err\n')
+            f.write('#SBATCH --cpus-per-task=1\n')
+            f.write('#SBATCH --array=0-199\n')
             f.write('python docker.py $SLURM_ARRAY_TASK_ID 200 --server $1 --exhaustiveness $2 --name $3 --oracle $4')
         
         # Trainer
         with open(os.path.join(script_dir, 'slurm_trainer.sh'), 'w') as f :
-            f.write('#!/bin/sh')
-            f.write('#SBATCH --account=def-jeromew')
-            f.write('#SBATCH --time=00:10:00')
-            f.write('#SBATCH --job-name=trainer') 
-            f.write('#SBATCH --output=out_slurm/trainer_%A.out')
-            f.write('#SBATCH --error=out_slurm/trainer_%A.err')
-            f.write('#SBATCH --gres=gpu:1') # gpu request   
+            f.write('#!/bin/sh\n')
+            f.write('#SBATCH --account=def-jeromew\n')
+            f.write('#SBATCH --time=00:10:00\n')
+            f.write('#SBATCH --job-name=trainer\n') 
+            f.write('#SBATCH --output=out_slurm/trainer_%A.out\n')
+            f.write('#SBATCH --error=out_slurm/trainer_%A.err\n')
+            f.write('#SBATCH --gres=gpu:1\n') # gpu request   
             f.write('python trainer.py --prior_name $1 --name $2 --iteration $3 --quantile $4 --uncertainty $5 --oracle $6')
         
     if args.server == 'cedar':
